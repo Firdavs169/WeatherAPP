c95abb124b3c1dba88d4a5a737cd44bf2621149b
Firdavs <firdavsmukhsimov@gmail.com>
2025-02-26 19:03:37 +0500

    Best version

--- Program.cs
+++ Program.cs
@@ -1,20 +1,23 @@
-﻿using System.Linq.Expressions;
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
 
-const string KEY = "9fc7ce0d68888e27260293804ed3fbf3";
+const string KEY = "3fe3618da3f5d90fb993d05aa32c0afc";
+Console.WriteLine("Please enter the city name: ");
+string city = Console.ReadLine();
 
-string input = Console.ReadLine()!;
-
-using (HttpClient client = new ())
+using (HttpClient client = new())
 {
-    try
+    try 
     {
-        string url = $"https://api.openweathermap.org/data/2.5/weather?q={input}&appid={KEY}";    
+        string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={KEY}";
         HttpResponseMessage response = await client.GetAsync(url);
+
 
         if(response.IsSuccessStatusCode)
         {
             string result = await response.Content.ReadAsStringAsync();
-            Console.WriteLine(result);
+            WeatherData weatherInfo = JsonSerializer.Deserialize<WeatherData>(result);
+            Console.WriteLine($"Shahar nomi: {weatherInfo.name}\nTemperature: {weatherInfo.main.Temp - 273.15f:F0}°C\nMa'lumot: {weatherInfo.weather[0].Description}");        
         }
         else
         {
@@ -23,7 +26,25 @@ using (HttpClient client = new ())
     }
     catch(Exception ex)
     {
-            Console.WriteLine($"Exception: {ex.Message}");
+        Console.WriteLine($"Exception: {ex.Message}");
     }
+}
 
+class WeatherData
+{
+    public string? name { get; set; } // to be fixed
+    public MainData? main { get; set; } // to be fixed
+    public WeatherInfo[]? weather { get; set; } // to be fixed
+}
+
+class MainData
+{
+    [JsonPropertyName("temp")]
+    public float Temp { get; set; }
+}
+
+class WeatherInfo
+{
+    [JsonPropertyName("description")]
+    public string? Description { get; set; }
 }
