29a65a98322ab1d23cc53f3c022c2606e23b1bb8
Firdavs <firdavsmukhsimov@gmail.com>
2025-02-27 21:13:06 +0500

    WeatherApp

---
+++ Models/WeatherData.cs
@@ -0,0 +1,6 @@
+class WeatherData
+{
+    public string? name { get; set; } // to be fixed
+    public MainData? main { get; set; } // to be fixed
+    public WeatherInfo[]? weather { get; set; } // to be fixed
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+
+const string KEY = "3fe3618da3f5d90fb993d05aa32c0afc";
+Console.WriteLine("Please enter the city name: ");
+string city = Console.ReadLine()!;
+
+using (HttpClient client = new())
+
+try 
+{
+    string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={KEY}";
+    HttpResponseMessage response = await client.GetAsync(url);
+
+
+    if(response.IsSuccessStatusCode)
+    {
+        string result = await response.Content.ReadAsStringAsync();
+        WeatherData weatherInfo = JsonSerializer.Deserialize<WeatherData>(result);
+        Console.WriteLine($"Shahar nomi: {weatherInfo.name}\nTemperature: {weatherInfo.main.Temp - 273.15f:F0}°C\nMa'lumot: {weatherInfo.weather[0].Description}");        
+    }
+    else
+    {
+        Console.WriteLine($"Error: {response.StatusCode}");
+    }
+}
+catch(Exception ex)
+{
+    Console.WriteLine($"Exception: {ex.Message}");
+}
